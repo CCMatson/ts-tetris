da8b523f2959fc359359e26ebd21de0b1eabc5da
CCMatson <catherine.matson@gmail.com>
2023-05-03 19:44:49 -0700

    create useInterval hook

--- src/hooks/useTetris.ts
+++ src/hooks/useTetris.ts
@@ -1,3 +1,28 @@
-// export function useTetris(){
-//   const [isPlaying, setIsPlaying] = useState(false)
-// }
+// import { Block, BlockShape, BoardShape, Shapes,EmptyCell } from "../types/types";
+import { useInterval } from "./useInterval";
+
+
+enum TickSpeed {
+  Normal = 800,
+}
+import { useState} from "react"
+
+export function useTetris(){
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [tickSpeed, setTickSpeed] = useState<TickSpeed | null>(null);
+
+  // const [
+  //   { board, droppingRow, droppingColumn, droppingBlock, droppingShape }, 
+  //   dispatchBoardState ] = useTetrisBoard();
+
+  //   const gameTick = useCallback(() => {
+  //     dispatchBoardState({type:'drop'})
+  //   }, [dispatchBoardState]);
+
+    useInterval(() => {
+      if(!isPlaying){
+        return
+      }
+      gameTick();
+    }, tickSpeed);
+}

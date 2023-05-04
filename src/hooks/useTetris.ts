a99b35aed9c95586c601df49540e4628030e9901
CCMatson <catherine.matson@gmail.com>
2023-05-04 10:57:51 -0700

    refactor useTetris and add helper function addShapeToBoard

--- src/hooks/useTetris.ts
+++ src/hooks/useTetris.ts
@@ -1,59 +1,110 @@
-import { useEffect , useState ,useCallback } from "react";
-import { Block, BlockShape, BoardShape, Shapes,EmptyCell } from "../types/types";
+import { useEffect, useState, useCallback } from "react";
+import { Block, BlockShape, BoardShape, Shapes, EmptyCell } from "../types/types";
 import { useInterval } from "./useInterval";
 import {
   useTetrisBoard,
   BOARD_HEIGHT,
   getEmptyBoard,
   getRandomBlock,
+  hasCollision,
 } from './useTetrisBoard';
 
 enum TickSpeed {
   Normal = 800,
+  Sliding = 100,
+  Fast = 50
 }
 
 export function useTetris() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [tickSpeed, setTickSpeed] = useState<TickSpeed | null>(null);
+  const [isCommiting, setIsCommiting] = useState(false)
 
   const [
-    { board, droppingRow, droppingColumn, droppingBlock, droppingShape }, 
-    dispatchBoardState 
+    { board, droppingRow, droppingColumn, droppingBlock, droppingShape },
+    dispatchBoardState
   ] = useTetrisBoard();
 
   const startGame = useCallback(() => {
-    setIsPlaying(true);
+    setIsCommiting(false)
+    setIsPlaying(true)
     setTickSpeed(TickSpeed.Normal)
-    dispatchBoardState({type: 'start'});
+    dispatchBoardState({ type: 'start' });
   }, [dispatchBoardState])
 
-    const gameTick = useCallback(() => {
-      dispatchBoardState({type:'drop'})
-    }, [dispatchBoardState]);
+  const commitPosition = useCallback(() => {
+    if (!hasCollision(board, droppingShape, droppingRow + 1, droppingColumn)) {
+      setIsCommiting(false);
+      setTickSpeed(TickSpeed.Normal)
+      return
+    }
 
-    useInterval(() => {
-      if(!isPlaying){
-        return
-      }
-      gameTick();
-    }, tickSpeed);
+    const newBoard = structuredClone(board) as BoardShape;
+    addShapeToBoard(
+      newBoard,
+      droppingBlock,
+      droppingShape,
+      droppingRow,
+      droppingColumn
+    )
+    setTickSpeed(TickSpeed.Normal)
+    dispatchBoardState({type: 'commit'})
+    setIsCommiting(false)
+  }, [board, dispatchBoardState, droppingBlock, droppingColumn, droppingRow, droppingShape])
 
-const renderedBoard = structuredClone(board) as BoardShape;
-if (isPlaying) {
+  const gameTick = useCallback(() => {
+    if (isCommiting) {
+      commitPosition();
+    } else if (
+      hasCollision(board, droppingShape, droppingRow + 1, droppingColumn)
+    ) {
+      setTickSpeed(TickSpeed.Sliding)
+      setIsCommiting(true)
+    } else {
+      dispatchBoardState({ type: 'drop' })
+    }
+  }, [board, commitPosition, dispatchBoardState, droppingColumn, droppingRow, droppingShape, isCommiting]);
+
+  useInterval(() => {
+    if (!isPlaying) {
+      return
+    }
+    gameTick();
+  }, tickSpeed);
+
+  const renderedBoard = structuredClone(board) as BoardShape;
+  if (isPlaying) {
+    addShapeToBoard(
+      renderedBoard,
+      droppingBlock,
+      droppingShape,
+      droppingRow,
+      droppingColumn,
+    )
+  }
+  return {
+    board: renderedBoard,
+    startGame,
+    isPlaying
+  }
+
+}
+
+function addShapeToBoard(
+  board: BoardShape,
+  droppingBlock: Block,
+  droppingShape: BlockShape,
+  droppingRow: number,
+  droppingColumn: number
+) {
   droppingShape
-  .filter((row) => row.some((isSet) => isSet))
-  .forEach((row: boolean[], rowIndex: number) => {
-    row.forEach((isSet: boolean, colIndex: number) => {
-      if (isSet) {
-        renderedBoard[droppingRow + rowIndex][droppingColumn + colIndex] = droppingBlock;
-      }    
-    });
-  })
-}
+    .filter((row) => row.some((isSet) => isSet))
+    .forEach((row: boolean[], rowIndex: number) => {
+      row.forEach((isSet: boolean, colIndex: number) => {
+        if (isSet) {
+          board[droppingRow + rowIndex][droppingColumn + colIndex] = droppingBlock;
+        }
+      });
+    })
 
-return {
-  board: renderedBoard,
-  startGame,
-  isPlaying
-}
 }

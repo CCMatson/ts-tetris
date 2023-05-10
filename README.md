b0fec89e5c604df0ac82f65c426f91dbda0fa290
CCMatson <catherine.matson@gmail.com>
2023-05-09 22:35:35 -0700

    update readme

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-# Tetris
+# TETRIS
 
-## This is a Vite app, made with React and TypeScript
+#[Click here to play](https://tetris-ts-23.netlify.app)
+
+## This is a Vite app, made with React and TypeScript. To play, use the arrow keys to move the blocks into completed lines.
+
+<img src="https://i.imgur.com/NyPyoaC.png" title="source: imgur.com" />
 
 ## IceBox
-[ ] Deployment
-[ ] Light and dark mode
+- [x] Deployment
+- [ ] Light and dark mode
+- [ ] Improve mobile game play
 
-<a href="https://www.flaticon.com/free-icons/tetris" title="tetris icons">Tetris icons created by cah nggunung - Flaticon</a>
+## Acknowledgements
+* Tetris is owned by the Tetris Company 
+* <a href="https://www.flaticon.com/free-icons/tetris" title="tetris icons">Tetris icons created by cah nggunung - Flaticon</a>
--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,23 @@ import UpcomingBlocks from "./components/upcomingBlocks";
 import { useTetris } from "./hooks/useTetris";
 
 function App(): JSX.Element {
-  const { board, startGame, isPlaying , score , upcomingBlocks} = useTetris();
+  const { board, startGame, isPlaying, score, upcomingBlocks } = useTetris();
 
   return (
-      <div className="App">
-        <h1>Tetris</h1>
-        <Board currentBoard={board} />
-        <div className="controls">
-          <h2>Score: {score}</h2>
-          {isPlaying ? (
-            <UpcomingBlocks upcomingBlocks={upcomingBlocks}/>
-            ): ( <button  className="button" onClick={startGame}>New Game</button>)}
-        </div>
+    <div className="app">
+      <h1>TETRIS</h1>
+      <Board currentBoard={board} />
+      <div className="controls">
+        <h2>Score: {score}</h2>
+        {isPlaying ? (
+          <UpcomingBlocks upcomingBlocks={upcomingBlocks} />
+        ) : (
+          <button className="button" onClick={startGame}>
+            New Game
+          </button>
+        )}
       </div>
+    </div>
   );
 }
 
